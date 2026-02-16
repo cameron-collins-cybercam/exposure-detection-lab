6ae2db91854ab2c8b38abbd9e79df659a84d3449
Cameron Collins <collinsexpense@gmail.com>
2026-02-15 18:31:54 -0600

    Add YAML template loading function

--- scanner/parser.py
+++ scanner/parser.py
@@ -1,4 +1,9 @@
 import re
+import yaml
+
+def load_template(path):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
 
 def check_pattern(content, pattern):
     if re.search(pattern, content):
